6b98da1bdba1859842bfe9de9242031c7ac2a362
Kalashnyk Bohdan <space.bohdan@gmail.com>
2020-07-27 01:12:20 +0300

    Updates

---
+++ precache-manifest.9ca3fdcb99612ab597d1e7ccaccdcde5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39901f448faf12d62b9ce417d9ceb75f",
+    "url": "/goit-react-hw-05-phonebook-ctx/index.html"
+  },
+  {
+    "revision": "291649a5277b22b6a9d9",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/css/2.2cf0c88b.chunk.css"
+  },
+  {
+    "revision": "33e21942c19ed06f61a7",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/css/main.d62c2529.chunk.css"
+  },
+  {
+    "revision": "291649a5277b22b6a9d9",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/2.44fffbb8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/2.44fffbb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33e21942c19ed06f61a7",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/main.706d50dd.chunk.js"
+  },
+  {
+    "revision": "54dd34004e0b6cdfb6f4",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/runtime-main.1121389a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-03-phonebook/precache-manifest.77314dd17243b1ec133a9fec0555e24c.js"
+  "/goit-react-hw-05-phonebook-ctx/precache-manifest.9ca3fdcb99612ab597d1e7ccaccdcde5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-03-phonebook/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-05-phonebook-ctx/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
